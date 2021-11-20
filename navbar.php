30853f249f18361bc1260f547750347be5b3a977
dishabaghele <94739282+dishabaghele@users.noreply.github.com>
2021-11-20 15:01:39 +0530

    Files uploaded

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php"><h5><strong>SF BANK</strong></h5></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php"><h5><strong>Home</strong></h5></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php"><h5><strong>Create User</strong></h5></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php"><h5><strong>Transfer Money</strong></h5></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php"><h5><strong>Transaction History</strong></h5></a>
+              </li>
+          </div>
+       </nav>
